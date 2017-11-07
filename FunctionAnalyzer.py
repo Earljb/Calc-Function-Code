fef9eee2bae61981a1d4b7ca9f2112eae6ef62b6
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-07 11:58:08 -0500

    Updated from Brython Server: 11/7/2017 11:58:08 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -27,11 +27,11 @@ POINTS OF INFLECTION
 """
 
 step = 1
-calc_precision = 0.01
+calc_precision = 0.00000001
 
 x_values = [x/step for x in list(range(0,3*step+1))]
 
 for x in x_values:
     f = x**2
     print(f,end='---')
-    #print(((x+calc_precision)**2-(x-calc_precision))/(2*calc_precision))
+    print(((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision))
