e005555982572c6a9262f259fa9128b57cb72f9a
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-07 11:56:38 -0500

    Updated from Brython Server: 11/7/2017 11:56:38 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -26,9 +26,12 @@ CONCAvITY
 POINTS OF INFLECTION
 """
 
-step = 1000
+step = 1
+calc_precision = 0.01
 
 x_values = [x/step for x in list(range(0,3*step+1))]
 
 for x in x_values:
-    print(x**2)
+    f = x**2
+    print(f,end='---')
+    #print(((x+calc_precision)**2-(x-calc_precision))/(2*calc_precision))
