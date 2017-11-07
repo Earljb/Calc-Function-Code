a8377fc79b965fb2bea770e52c232297bfd658f2
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-07 12:03:22 -0500

    Updated from Brython Server: 11/7/2017 12:03:21 PM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -10,6 +10,19 @@ We need to give an input of a function and output:
 term=input("How many terms do you want in your function?")
 """
 
+step = 1
+calc_precision = 0.0001
+
+x_values = [x/step for x in list(range(-3,3*step+1))]
+
+f_data = [(x_values[i],0,0) for i in len(x_values)]
+print(f_data)
+
+for x in x_values:
+    f = x**2
+    print(f,end='  <>  ')
+    print(((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision))
+
 """
 EXTREMA
 """
@@ -26,12 +39,3 @@ CONCAvITY
 POINTS OF INFLECTION
 """
 
-step = 1
-calc_precision = 0.00000001
-
-x_values = [x/step for x in list(range(0,3*step+1))]
-
-for x in x_values:
-    f = x**2
-    print(f,end='---')
-    print(((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision))
