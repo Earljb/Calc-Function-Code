bf0437c236a0e8b569f5da5fdee20bc6f3848b7b
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-11-07 11:46:13 -0500

    Updated from Brython Server: 11/7/2017 11:46:12 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -1,10 +1,13 @@
 """
 We need to give an input of a function and output:
--extrema (max/min)
+-extrema (max/min, local/absolute)
 -inc/dec intervals
 -concave up/down
 -points of inflection
 """
+
+"""
 term=input("How many terms do you want in your function?")
-if term == 1
-    
+"""
+
+"""
