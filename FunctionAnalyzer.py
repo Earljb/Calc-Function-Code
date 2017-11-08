6753b20c315823e227693ea151071359a80b4ec2
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-11-08 11:26:32 -0500

    Updated from Brython Server: 11/8/2017 11:26:31 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -19,7 +19,8 @@ x_values = [x/step for x in list(range(-3*step,3*step+1))]
 f_data = []
 
 for x in x_values:
-    fp = ((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision)
+    #fp = ((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision)
+    fp = ((x+calc_precision)**3-(x+calc_precision)-(((x-calc_precision)**2)-(x-calc_precision)))/(2*calc_precision)
     f_data.append([x,x**2,fp])
     
 print(f_data)
@@ -32,8 +33,8 @@ for i in range(len(f_data)-2):
         print("Local min at "+str(f_data[i+1][0]))
     elif f_data[i][2] > 0 and f_data[i+2][2] < 0:
         print("Local max at "+str(f_data[i+1][0]))
-    else:
-        print("There are no local max's in the interval")
+    #else:
+    #    print("There are no local max's in the interval")
 """
 """
 
