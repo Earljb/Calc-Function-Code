07f053a1e0d89a7f02eb19ba676c4fc013c0cf78
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-11-08 11:35:27 -0500

    Updated from Brython Server: 11/8/2017 11:35:26 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -11,6 +11,8 @@ term = input("How many terms do you want in your function?")
 interval = input("What interval do you want us to analyze?"
 """
 
+from math import sin
+
 step = 100
 calc_precision = 0.0001
 
@@ -20,7 +22,7 @@ f_data = []
 
 for x in x_values:
     #fp = ((x+calc_precision)**2-(x-calc_precision)**2)/(2*calc_precision)
-    fp = ((x+calc_precision)**3-(x+calc_precision)-(((x-calc_precision)**3)-(x-calc_precision)))/(2*calc_precision)
+    #fp = ((x+calc_precision)**3-(x+calc_precision)-(((x-calc_precision)**3)-(x-calc_precision)))/(2*calc_precision)
     f_data.append([x,x**3-x,fp])
     
 print(f_data)
@@ -29,10 +31,10 @@ EXTREMA
 """
 
 for i in range(len(f_data)-2):
-    if f_data[i][2] < 0 and f_data[i][2] <= 0 and f_data[i+2][2] > 0:
-        print("Local min at "+str(f_data[i+1][0]))
-    elif f_data[i][2] > 0 and f_data[i][2] >= 0 and f_data[i+2][2] < 0:
-        print("Local max at "+str(f_data[i+1][0]))
+    if f_data[i][2] < 0 and f_data[i+1][2] <= 0 and f_data[i+2][2] > 0:
+        print("Local min at x = "+str(f_data[i+1][0]))
+    elif f_data[i][2] > 0 and f_data[i+1][2] >= 0 and f_data[i+2][2] < 0:
+        print("Local max at x = "+str(f_data[i+1][0]))
     #else:
     #    print("There are no local max's in the interval")
 """
