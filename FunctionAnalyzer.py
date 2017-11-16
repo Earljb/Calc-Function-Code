59820a4c638abad9b194cc96637883ebc8a13461
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-11-16 12:03:35 -0500

    Updated from Brython Server: 11/16/2017 12:03:34 PM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -46,16 +46,16 @@ INC/DEC INTERVALS
 d_interval = []
 i_interval = []
 
-for i in range(len(f_data)):
+for i in range(len(f_data)-2):
     if f_data[i][2] > 0 and f_data[i+1][2] >= 0 and f_data[i+2][2] < 0:
         d_start = f_data[i][0]
     elif f_data[i][2] < 0 and f_data[i+1][2] <= 0 and f_data[i+2][2] > 0:
         d_stop = f_data[i][0]
-    d_interval.append(d_start,d_stop)
+    d_interval = [d_start,d_stop]
 
 #    elif f_data[i][2] > 0 and f_data[i+1][2] > 0:
 #        i_interval.append(f_data[i][0])
-#print("There is a decreasing interval on ["+str(d_interval)+"]")
+print("There is a decreasing interval on ["+str(d_interval)+"]")
 #print("There is an increasing interval on ["+str(i_interval[0])+", "+str(i_interval[-1])+"]")
 
 """
