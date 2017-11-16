de5e94f61139d37c785be3ff005e0ca90b6b4186
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-11-16 11:55:25 -0500

    Updated from Brython Server: 11/16/2017 11:55:24 AM

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -48,14 +48,15 @@ i_interval = []
 
 
 
-for i in range(len(f_data)-2):
-    if f_data[i][2] < 0:
-        for i in range(len(f_data)):
-            if f_data[i][2] < f_data[i+1][2] and f_data[i][2] <= f_data[i+1][2]:
-                d_start = f_data[i][0]
-            elif f_data[i+1][2] < f_data[i][2] and f_data[i+1][2] <= f_data[i][2]:
-                d_stop = f_data[i][0]
-        d_interval.append(d_start,d_stop)
+#for i in range(len(f_data)-2):
+#    if f_data[i][2] < 0:
+    
+for i in range(len(f_data)):
+    if f_data[i][2] < f_data[i+1][2] and f_data[i][2] <= f_data[i+1][2]:
+        d_start = f_data[i][0]
+    elif f_data[i+1][2] < f_data[i][2] and f_data[i+1][2] <= f_data[i][2]:
+        d_stop = f_data[i][0]
+    d_interval.append(d_start,d_stop)
 #    elif f_data[i][2] > 0 and f_data[i+1][2] > 0:
 #        i_interval.append(f_data[i][0])
 print("There is a decreasing interval on ["+str(d_interval)+"]")
